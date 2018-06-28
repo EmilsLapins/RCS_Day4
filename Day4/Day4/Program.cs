88ad68dc01a5d050948946f6040ea32f503b9562
Emils Lapins <emilslapins@gmail.com>
2018-06-28 20:31:56 +0300

    added switch

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -10,6 +10,10 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            //
+            //     if else statements !
+            //
+
             Console.WriteLine("1, ja izvelies izsaukt Uzdevums klasi; 2, ja Zarosanas klasi!");
             string input = Console.ReadLine();
            
@@ -36,6 +40,34 @@ namespace Day4
                 }
                 
             }
+            //
+            // versija ar Switch statement!
+            //
+            switch(input)
+            {
+                case "1":
+
+                    Console.WriteLine("Izvelejaties Uzdevums klasi!");
+                    Uzdevums uzdevums1 = new Uzdevums(); // konstruejam objektu!
+                    uzdevums1.Publiska(); // izsaucam no uzdevums klases public funkciju!
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("Izvelejaties Zarosanas klasi!");
+                    Zarosanas zari = new Zarosanas(); // ja ievaditais ir mazaks par 5
+                    zari.LielaksVaiMazaks();
+
+                    break;
+                default:
+                    Console.WriteLine("Nemakam lasit?");
+                    break; // ar break mes izejam ara no switch statement!
+
+
+                   
+
+
+            }   
             
 
 
