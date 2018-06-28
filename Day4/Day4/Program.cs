9a5ca544d4175f191839f64ebb522f64a9777f06
Emils Lapins <emilslapins@gmail.com>
2018-06-28 20:06:03 +0300

    123

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -10,14 +10,33 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Skaitli ludzu");
+            string abc = Console.ReadLine();
+            int abc1 = Convert.ToInt16(abc);
 
-            //  Uzdevums uzdevums1 = new Uzdevums(); // konstruejam objektu!
-            //  uzdevums1.Publiska(); // izsaucam no uzdevums klases public funkciju!
+            if (abc1 >= 5) // ja ievaditais ir lielaks par 5 vai 5, tad uzsaucam uzdevums klasi
+            {
+                Console.WriteLine("Vienads ar 5 , vai lielaks par 5");
+                Uzdevums uzdevums1 = new Uzdevums(); // konstruejam objektu!
+                uzdevums1.Publiska(); // izsaucam no uzdevums klases public funkciju!
+            }
+            else 
+            {
+                Console.WriteLine("Mazaks par 5");
+                Zarosanas zari = new Zarosanas(); // ja ievaditais ir mazaks par 5
+                zari.LielaksVaiMazaks();
+            }
 
-            // 
+           
+            
 
-            Zarosanas zari = new Zarosanas();
-            zari.LielaksVaiMazaks();
+
+
+
+
+            // cilveks var izveleties kuru funkciju izsaukt! <5 un >5 - divi gadijumi!
+
+
 
 
             Console.ReadLine();
