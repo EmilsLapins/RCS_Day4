00c82b7a57d29b1530a301edf9456b2eaf085065
Emils Lapins <emilslapins@gmail.com>
2018-06-28 20:14:31 +0300

    reworked if else statement

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -10,33 +10,33 @@ namespace Day4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skaitli ludzu");
+            Console.WriteLine("1, ja izvelies izsaukt Uzdevums klasi; 2, ja Zarosanas klasi!");
             string abc = Console.ReadLine();
             int abc1 = Convert.ToInt16(abc);
 
-            if (abc1 >= 5) // ja ievaditais ir lielaks par 5 vai 5, tad izsaucam uzdevums klasi
+            if (abc1 == 1) // ja ievaditais ir lielaks par 5 vai 5, tad izsaucam uzdevums klasi
             {
-                Console.WriteLine("Vienads ar 5 , vai lielaks par 5");
+                Console.WriteLine("Izvelejaties Uzdevums klasi!");
                 Uzdevums uzdevums1 = new Uzdevums(); // konstruejam objektu!
                 uzdevums1.Publiska(); // izsaucam no uzdevums klases public funkciju!
             }
-            else 
+            else
             {
-                Console.WriteLine("Mazaks par 5");
-                Zarosanas zari = new Zarosanas(); // ja ievaditais ir mazaks par 5
-                zari.LielaksVaiMazaks();
-            }
-
            
+                if (abc1 == 2)
+                {
+                    Console.WriteLine("Izvelejaties Zarosanas klasi!");
+                    Zarosanas zari = new Zarosanas(); // ja ievaditais ir mazaks par 5
+                    zari.LielaksVaiMazaks();
+                }
+                else
+                {
+                    Console.WriteLine("Nemakam lasit?");
+                   
+                }
+                
+            }
             
-
-
-
-
-
-            // cilveks var izveleties kuru funkciju izsaukt! <5 un >5 - divi gadijumi!
-
-
 
 
             Console.ReadLine();
